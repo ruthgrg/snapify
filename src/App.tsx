58e3e9b6329894b963368d541c865de2ec5ca31d
ruthgrg <gurungruth2@gmail.com>
2023-11-16 17:29:19 +0100

    meta tags added

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,11 @@ import CreatePost from "./_root/pages/CreatePost";
 import Entry from "./_auth/forms/Entry";
 import { useUserContext } from "./context/AuthContext";
 import { Loader } from "lucide-react";
+import { HelmetProvider } from "react-helmet-async";
 
 const App = () => {
+  const helmetContext = {};
+
   const userCtx = useUserContext();
   if (userCtx.isLoading) {
     return (
@@ -31,31 +34,33 @@ const App = () => {
   }
 
   return (
-    <main className="flex h-screen">
-      <Routes>
-        {/* public routes */}
-        <Route element={<AuthLayout />}>
-          <Route index element={<Entry />} />
-          <Route path="/sign-in" element={<SigninForm />} />
-          <Route path="/sign-up" element={<SignupForm />} />
-        </Route>
+    <HelmetProvider context={helmetContext}>
+      <main className="flex h-screen">
+        <Routes>
+          {/* public routes */}
+          <Route element={<AuthLayout />}>
+            <Route index element={<Entry />} />
+            <Route path="/sign-in" element={<SigninForm />} />
+            <Route path="/sign-up" element={<SignupForm />} />
+          </Route>
 
-        {/* private routes */}
-        <Route element={<RootLayout />}>
-          <Route path="/home" element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/saved" element={<Saved />} />
-          <Route path="/create-post" element={<CreatePost />} />
-          <Route path="/update-post/:id" element={<EditPost />} />
-          <Route path="/posts/:id" element={<PostDetails />} />
-          <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/update-profile/:id" element={<UpdateProfile />} />
-          <Route path="/all-users" element={<AllUsers />} />
-        </Route>
-      </Routes>
+          {/* private routes */}
+          <Route element={<RootLayout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/saved" element={<Saved />} />
+            <Route path="/create-post" element={<CreatePost />} />
+            <Route path="/update-post/:id" element={<EditPost />} />
+            <Route path="/posts/:id" element={<PostDetails />} />
+            <Route path="/profile/:id/*" element={<Profile />} />
+            <Route path="/update-profile/:id" element={<UpdateProfile />} />
+            <Route path="/all-users" element={<AllUsers />} />
+          </Route>
+        </Routes>
 
-      <Toaster />
-    </main>
+        <Toaster />
+      </main>
+    </HelmetProvider>
   );
 };
 
--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,4 +1,5 @@
 import { useUserContext } from "@/context/AuthContext";
+import Seo from "@/seo/Seo";
 import { useEffect } from "react";
 
 import { Outlet, useNavigate } from "react-router-dom";
@@ -14,19 +15,27 @@ const AuthLayout = () => {
   }, [navigate, userCtx.isAuthenticated]);
 
   return (
-    <div className="flex w-full">
-      <div className="flex flex-1 w-full ">
-        <section className="flex flex-1 justify-center items-center flex-col py-10">
-          <Outlet />
-        </section>
+    <>
+      <Seo
+        title="Social media app"
+        description="Snapgram is a dynamic and visually stunning React-powered social media app, offering a seamless blend of captivating photo sharing and engaging community interactions. With a user-friendly interface crafted in React, Snapgram lets you effortlessly capture, customize, and share your moments with friends and followers. Immerse yourself in a vibrant network where creativity knows no bounds. Elevate your social media experience with Snapgram, your personalized visual storytelling platform"
+        name="Snapgram"
+        type="Social media"
+      />
+      <div className="flex w-full">
+        <div className="flex flex-1 w-full ">
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
+            <Outlet />
+          </section>
 
-        <img
-          src="/assets/images/side-img.svg"
-          alt="logo"
-          className="h-screen w-1/2  object-cover bg-no-repeat  max-xl:hidden "
-        />
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="h-screen w-1/2  object-cover bg-no-repeat  max-xl:hidden "
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
