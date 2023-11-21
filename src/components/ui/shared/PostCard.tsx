ba832dd6e5eb136f5f1d8a40c55fcc882faabc8c
Ruth Gurung <gurungruth2@gmail.com>
2023-11-21 14:47:07 +0100

    image credential fixed

--- src/components/ui/shared/PostCard.tsx
+++ src/components/ui/shared/PostCard.tsx
@@ -61,6 +61,7 @@ const PostCard = ({ post }: PostCardProps) => {
           src={post.imageUrl || "/assets/icons/profile-placeholder.svg"}
           alt="post Image"
           className="h-64 xs:h-[400px] lg:h-[450px] w-full rounded-[24px] object-cover mb-5"
+          crossOrigin="use-credentials"
         />
       </Link>
       <PostStats post={post} userId={userCtx.user.id} />
