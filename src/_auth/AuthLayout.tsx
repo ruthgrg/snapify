daf5079c1a2eab33f89a8d630b057bbadf7e3cd5
Ruth Gurung <gurungruth2@gmail.com>
2023-11-29 18:21:56 +0100

    minor fixes

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -38,7 +38,7 @@ const AuthLayout = () => {
           </section>
 
           <img
-            src="/assets/images/side-img.svg"
+            src="/assets/images/side-img1.jpeg"
             alt="logo"
             className="h-screen w-1/2  object-cover bg-no-repeat  max-xl:hidden "
           />
